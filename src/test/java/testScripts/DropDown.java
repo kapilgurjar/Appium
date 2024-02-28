1b369aaf0b13a09de2ec8a47121b4dd6f925244b
kapilcsit@gmail.com <kapil.kumar2@rsystems.com>
2024-02-28 13:11:27 +0530

    Added dropdown and ios device code

---
+++ src/test/java/testScripts/DropDown.java
@@ -0,0 +1,57 @@
+package testScripts;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class DropDown {
+	
+AndroidDriver driver;
+	
+	@BeforeMethod
+	public void setUp() {
+		
+		UiAutomator2Options options = new UiAutomator2Options();
+		//options.setDeviceName("Android device");
+		options.setDeviceName("Pixel7");
+		options.setApp("C://Users//Kapil//eclipse-workspace//TestAutomation//resources//General-Store.apk");
+		try {
+			driver = new AndroidDriver(new URL("http://0.0.0.0:4723"), options);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void test01() throws InterruptedException {
+		Thread.sleep(5000);
+		driver.findElement(By.id("com.androidsample.generalstore:id/spinnerCountry")).click();
+		
+		//WebElement ele=driver.findElement(By.xpath("//android.widget.TextView[@text='India']"));
+		
+		//Actions action = new Actions(driver);
+		//action.moveToElement(ele).click().build().perform();
+		WebElement ele=driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"India\"));"));
+		Thread.sleep(5000);
+		ele.click();
+	
+	}
+	
+	
+	//@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
--- src/test/java/testScripts/FirstTest.java
+++ src/test/java/testScripts/FirstTest.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
@@ -15,10 +17,10 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 public class FirstTest {
 
 	AndroidDriver driver;
-
-	@Test
-	public void TC01_LaunchApp() {
-
+	
+	@BeforeMethod
+	public void setUp() {
+		
 		UiAutomator2Options options = new UiAutomator2Options();
 		options.setDeviceName("Pixel7");
 		options.setApp("C://Users//Kapil//eclipse-workspace//TestAutomation//resources//ApiDemos-debug.apk");
@@ -28,6 +30,19 @@ public class FirstTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	@Test
+	public void TC01_LaunchApp() {
+
+//		UiAutomator2Options options = new UiAutomator2Options();
+//		options.setDeviceName("Pixel7");
+//		options.setApp("C://Users//Kapil//eclipse-workspace//TestAutomation//resources//ApiDemos-debug.apk");
+//		try {
+//			driver = new AndroidDriver(new URL("http://0.0.0.0:4723"), options);
+//		} catch (MalformedURLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		//WebElement ele = driver.findElement(AppiumBy.accessibilityId("App"));
 		//ele.click();
 		System.out.println(driver.getBatteryInfo().getLevel());
@@ -35,17 +50,26 @@ public class FirstTest {
 				By.xpath("//android.widget.ListView[@resource-id=\"android:id/list\"]/android.widget.TextView"));
 		int size = listele.size();
 		System.out.println(size);
-		
 		for(int i=0;i<size;i++) {
 			String text=listele.get(i).getAttribute("text");
 			System.out.println(text);
-			
 			if(text.equalsIgnoreCase("OS")) {
 				listele.get(i).click();
 				break;
 			}
 		}
 
+		
+	}
+	@Test
+	public void TC02() {
+		
+		driver.findElement(AppiumBy.accessibilityId("App")).click();
+		
+	}
+	
+	@AfterMethod
+	public void tearDown() {
 		driver.quit();
 	}
 
